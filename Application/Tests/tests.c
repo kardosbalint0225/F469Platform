0cbcab1e19fdccf234efce581731bbbdb073d287
Kardos Balint <36537345+kardosbalint0225@users.noreply.github.com>
2023-08-24 16:46:21 +0200

    retarget_locks added. Started implementation of syscalls hooks.

--- Application/Tests/tests.c
+++ Application/Tests/tests.c
@@ -14,15 +14,17 @@
 
 #include "uart_console.h"
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-static char *test_strings[6] = {
-    "This is a text message.\r\n",
-    "Thats another text message.\r\n",
-    "I am not very creative when it comes to writing test text messages.\r\n",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\r\n",
-    "Contrary to popular belief, Lorem Ipsum is not simply random text.\r\n",
-    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.\r\n",
-};
+//static char *test_strings[6] = {
+//    "This is a text message.\r\n",
+//    "Thats another text message.\r\n",
+//    "I am not very creative when it comes to writing test text messages.\r\n",
+//    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\r\n",
+//    "Contrary to popular belief, Lorem Ipsum is not simply random text.\r\n",
+//    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.\r\n",
+//};
 
 static void tests_runner_task(void * params);
 
@@ -42,18 +44,54 @@ static void tests_runner_task(void * params)
 {
     for ( ;; )
     {
-        for (int i = 0; i < 10; i++)
-        {
-            int size = 6;
-            for (int i = 0; i < size; i++)
-            {
-                char *pbuf = test_strings[i];
-                int len = strlen(pbuf);
-                __uart_console_write(pbuf, len);
-            }
-            vTaskDelay(500);
-        }
-        assert_param(0);
+//        for (int i = 0; i < 10; i++)
+//        {
+//            int size = 6;
+//            for (int i = 0; i < size; i++)
+//            {
+//                char *pbuf = test_strings[i];
+//                int len = strlen(pbuf);
+//                __uart_console_write(pbuf, len);
+//            }
+//            vTaskDelay(500);
+//        }
+//        assert_param(0);
+        extern uint8_t _end; /* Symbol defined in the linker script */
+        extern uint8_t _estack; /* Symbol defined in the linker script */
+        extern uint32_t _Min_Stack_Size; /* Symbol defined in the linker script */
+        const uint32_t stack_limit = (uint32_t)&_estack - (uint32_t)&_Min_Stack_Size;
+        const size_t max_heap_size = (stack_limit - (uint32_t)&_end) >> 1;
+        uint8_t *pbuf = (uint8_t *)malloc(max_heap_size);
+        assert_param(NULL != pbuf);
+        free(pbuf);
+        tzset();
+
+        char *env = NULL;
+        setenv("ENVVAR1", "1", 1);
+        env = getenv("ENVVAR1");
+        __uart_console_write(env, strlen(env));
+
+        setenv("ENVVAR2", "2", 1);
+        env = getenv("ENVVAR2");
+        __uart_console_write(env, strlen(env));
+
+        setenv("ENVVAR3", "3", 1);
+        env = getenv("ENVVAR3");
+        __uart_console_write(env, strlen(env));
+
+        setenv("ENVVAR4", "4", 1);
+        env = getenv("ENVVAR4");
+        __uart_console_write(env, strlen(env));
+
+        setenv("ENVVAR5", "5", 1);
+        env = getenv("ENVVAR5");
+        __uart_console_write(env, strlen(env));
+
+        env = getenv("ENVVAR_NONEXISTING");
+        if (NULL != env)
+            __uart_console_write(env, strlen(env));
+
+        vTaskDelay(10);
     }
 }
 
---
+++ Core/Src/retarget_locks.c
@@ -0,0 +1,102 @@
+/*
+ * retarget_locks.c
+ *
+ *  Created on: 2023. aug. 23.
+ *      Author: Balint
+ */
+#include <sys/lock.h>
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
+struct __lock {
+    SemaphoreHandle_t   sem;
+};
+
+struct __lock __lock___sinit_recursive_mutex;
+struct __lock __lock___sfp_recursive_mutex;
+struct __lock __lock___atexit_recursive_mutex;
+struct __lock __lock___at_quick_exit_mutex;
+struct __lock __lock___malloc_recursive_mutex;
+struct __lock __lock___env_recursive_mutex;
+struct __lock __lock___tz_mutex;
+struct __lock __lock___dd_hash_mutex;
+struct __lock __lock___arc4random_mutex;
+
+
+__attribute__((constructor))
+static void init_retarget_locks(void)
+{
+    __lock___sinit_recursive_mutex.sem  = xSemaphoreCreateRecursiveMutex();
+    __lock___sfp_recursive_mutex.sem    = xSemaphoreCreateRecursiveMutex();
+    __lock___atexit_recursive_mutex.sem = xSemaphoreCreateRecursiveMutex();
+    __lock___at_quick_exit_mutex.sem    = xSemaphoreCreateMutex();
+    __lock___malloc_recursive_mutex.sem = xSemaphoreCreateRecursiveMutex();
+    __lock___env_recursive_mutex.sem    = xSemaphoreCreateRecursiveMutex();
+    __lock___tz_mutex.sem               = xSemaphoreCreateMutex();
+    __lock___dd_hash_mutex.sem          = xSemaphoreCreateMutex();
+    __lock___arc4random_mutex.sem       = xSemaphoreCreateMutex();
+}
+
+void __retarget_lock_init(_LOCK_T *lock_ptr)
+{
+    *lock_ptr = pvPortMalloc(sizeof(struct __lock));
+    (*lock_ptr)->sem = xSemaphoreCreateMutex();
+}
+
+
+void __retarget_lock_init_recursive(_LOCK_T *lock_ptr)
+{
+    *lock_ptr = pvPortMalloc(sizeof(struct __lock));
+    (*lock_ptr)->sem = xSemaphoreCreateRecursiveMutex();
+}
+
+
+void __retarget_lock_close(_LOCK_T lock)
+{
+    vSemaphoreDelete(lock->sem);
+    vPortFree(lock);
+}
+
+
+void __retarget_lock_close_recursive(_LOCK_T lock)
+{
+    vSemaphoreDelete(lock->sem);
+    vPortFree(lock);
+}
+
+
+void __retarget_lock_acquire(_LOCK_T lock)
+{
+    xSemaphoreTake(lock->sem, portMAX_DELAY);
+}
+
+
+void __retarget_lock_acquire_recursive(_LOCK_T lock)
+{
+    xSemaphoreTakeRecursive(lock->sem, portMAX_DELAY);
+}
+
+
+int __retarget_lock_try_acquire(_LOCK_T lock)
+{
+    return xSemaphoreTake(lock->sem, 0);
+}
+
+
+int __retarget_lock_try_acquire_recursive(_LOCK_T lock)
+{
+    return xSemaphoreTakeRecursive(lock->sem, 0);
+}
+
+
+void __retarget_lock_release(_LOCK_T lock)
+{
+    xSemaphoreGive(lock->sem);
+}
+
+
+void __retarget_lock_release_recursive(_LOCK_T lock)
+{
+    xSemaphoreGiveRecursive(lock->sem);
+}
