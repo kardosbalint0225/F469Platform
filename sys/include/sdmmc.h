7f0275465c22ad73509c67c88af659af4eb0b3a5
Kardos Balint <36537345+kardosbalint0225@users.noreply.github.com>
2023-10-15 13:55:37 +0200

    Started the implementation of sdmmc

---
+++ sys/include/sdmmc.h
@@ -0,0 +1,13 @@
+/*
+ * sdmmc.h
+ *
+ *  Created on: 2023. okt. 15.
+ *      Author: Balint
+ */
+
+#ifndef _SDMMC_H_
+#define _SDMMC_H_
+
+
+
+#endif /* INCLUDE_SDMMC_H_ */
---
+++ sys/sdmmc/sdmmc.c
@@ -0,0 +1,9 @@
+/*
+ * sdmmc.c
+ *
+ *  Created on: 2023. okt. 15.
+ *      Author: Balint
+ */
+#include "sdmmc.h"
+
+
