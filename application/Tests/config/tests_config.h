c2598a8e3fb3a33df76efe6cb3cd5ccc9434f6ed
Kardos Balint <36537345+kardosbalint0225@users.noreply.github.com>
2024-02-24 10:47:22 +0100

    application folder readded

---
+++ application/Tests/config/tests_config.h
@@ -0,0 +1,6 @@
+#ifndef __TESTS_CONFIG_H__
+#define __TESTS_CONFIG_H__
+
+
+#endif /* __TESTS_CONFIG_H__ */
+
---
+++ application/main.c
@@ -0,0 +1,19 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "stm32f4xx_hal.h"
+#include "system_clock.h"
+
+int main(void)
+{
+    HAL_Init();
+    system_clock_init();
+
+    vTaskStartScheduler();
+
+    while (1)
+    {
+
+    }
+}
+
